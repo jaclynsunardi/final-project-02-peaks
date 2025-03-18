5a5c9287a5a5d63cf1a89a4ca7903a74832fdfbc
Jaclyn Sunardi <jsa411@sfu.ca>
2025-03-17 23:06:51 -0700

    fourth time trying automated testing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Miso Hungry!
+          Learn React
         </a>
       </header>
     </div>
