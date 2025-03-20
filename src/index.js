e67d4331e3ec9673d40cb9e55175f9760228740b
elenaktnguyen <elenanguyen2005@gmail.com>
2025-03-20 13:50:06 -0700

    chore: clean comments for simplicity

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // Import RouterProvider
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'; 
 
 import App from './App.jsx';
 import './index.css';
@@ -9,7 +9,6 @@ import Form from './pages/Form.jsx';
 import Learn from './pages/Learn.jsx';
 import Recipe from './pages/Recipe.jsx';
 
-// Define the router
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
   { path: '/index', element: <Homepage /> },
@@ -18,9 +17,8 @@ const router = createBrowserRouter([
   { path: '/recipe', element: <Recipe /> }
 ]);
 
-// Render the app with the RouterProvider to manage the routes
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} /> {/* Use RouterProvider instead of BrowserRouter */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
