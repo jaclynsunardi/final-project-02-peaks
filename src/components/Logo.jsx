05f290a866499ed0f243ed4cafe68db97cc6448e
elenaktnguyen <elenanguyen2005@gmail.com>
2025-03-29 04:41:47 -0700

    style: add images and style web application

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import LogoImage from '../images/logoImage.png';
 
 function Logo() {
   return (
     <>
-        <div className="logo-container">
-            <h1><NavLink to = '/index' className={({ isActive }) => isActive ? "active-link" : ""}>Logo</NavLink></h1>
-        </div>
+      <div className="logo-container">
+        <h1>
+          <NavLink 
+            to='/index' 
+            className={({ isActive }) => isActive ? 'active-link' : ''}
+          >
+            <img 
+              src={LogoImage} 
+              alt="MisoHungry Logo" // Alt text for the image
+              className="logo-image" // Optional: add a class to style the image
+            />
+          </NavLink>
+        </h1>
+      </div>
     </>
   );
 }
